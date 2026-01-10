e129a10d740c7f1bed929d3cb15402f405242d7b
kauatorresfranca <kauatorresfranca2@gmail.com>
2026-01-10 14:08:51 -0300

    fix: add vite types to tsconfig.app.json

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+// O TypeScript agora reconhece VITE_API_URL graças ao arquivo .d.ts
+const apiURL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api';
+
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api', // URL do seu Django
+  baseURL: apiURL,
 });
 
 export default api;
