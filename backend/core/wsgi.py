d03ada2bbe08c1575ceec2dcd4971564cf837e7b
kauatorresfranca <kauatorresfranca2@gmail.com>
2026-01-10 14:40:50 -0300

    feat: auto migrate and import data on startup

--- backend/core/wsgi.py
+++ backend/core/wsgi.py
@@ -1,16 +1,15 @@
-"""
-WSGI config for core project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
+
+try:
+    print("Iniciando migrações e importação automática...")
+    call_command('migrate')
+    call_command('import_data')
+    print("Importação concluída com sucesso!")
+except Exception as e:
+    print(f"Erro na importação automática: {e}")
